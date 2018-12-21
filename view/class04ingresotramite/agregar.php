3916918821e21038a24bc6cc1ea152c8d01877d2
Mauricio Chévez Gutiérrez <30674987+MauricioChevezG@users.noreply.github.com>
2018-12-20 22:12:33 -0600

    Cambios

--- view/class04ingresotramite/agregar.php
+++ view/class04ingresotramite/agregar.php
@@ -4,131 +4,115 @@
 <?php $result4 = $this->pu26planreg2->listarSamara(); ?>
 <?php $result5 = $this->pu13aap->listar(); ?>
 
+<div class="alert alert-warning alert-dismissable">
+  <button type="button" class="close" data-dismiss="alert">&times;</button>
+  <strong>¡ Si estas conectado desde un celular o tablet, preferiblemente utilícelo en forma horizontal !</strong>
+</div>
+<center><h2>Agregar Nuevo Trámite</h2></center>
+<form action="?c=class04ingresotramite&m=agregar" enctype="multipart/form-data" method="post" >
 
-
-   <div class="container-fluid">
-     <div class="row">
-     <div class="alert alert-success alert-dismissable">
-    <button type="button" class="close" data-dismiss="alert">&times;</button>
-    <strong>¡ Si estas conectado desde un celular o tablet, preferiblemente utilícelo en forma horizontal !</strong>
-    </div>
-  <center><h2>Agregar Nuevo Trámite</h2></center>
-    <form action="?c=class04ingresotramite&m=agregar" enctype="multipart/form-data" method="post" >
+  <div class="container-fluid">
+    <div class="col-sm-offset-4 col-sm-4">
       <div class="form-group row">
-        <div class="col-md-3">
+
         <label for="PU04IDTRA">Código del Trámite:</label>
         <input type="text" class="form-control" id="PU04IDTRA" name="PU04IDTRA">
-        </div>
+
       </div>
 
       <div class="container-fluid">
-      <div class="form-group row">
-      <div class="col-xs-3">
-      <label for="PU04FETRA">Fecha de Revisión de Expediente:</label>
-      <input type="date" class="form-control" name="PU04FETRA" id="">
-      </div>
-      </div>
-      </div>
+        <div class="form-group row">
 
+          <label for="PU04FETRA">Fecha de Revisión de Expediente:</label>
+          <input type="date" class="form-control" name="PU04FETRA" id="">
 
-      <!--<div class="form-group row">
-           <div class="col-md-3">
-          <label for="PU04IDDISTRITO">Distrito:</label>
-          <input type="text" class="form-control" id="PU04IDDISTRITO" name="PU04IDDISTRITO">
-          </div>
-      </div>
-  -->
-  <div class="form-group">
-        <div class="col-md-3 col-sm-3 col-xs-3">
-          <label for="PU04IDDISTRITO">Distrito al que pertenece: </label>
-          <select class="form-control selectpicker" name="PU04IDDISTRITO" id="PU04IDDISTRITO">
-            <option value="Seleccione">Seleccione...</option>
-            <option value="Nicoya">Nicoya</option>
-            <option value="Masión">Masión</option>
-            <option value="San Antonio">San Antonio</option>
-            <option value="Quebrada Honda">Quebrada Honda</option>
-            <option value="Sámara">Sámara</option>
-            <option value="Nosara">Nosara</option>
-            <option value="Belén">Belén</option>
-          </select>
         </div>
       </div>
-      <br> <br> <br>
-       <br> <br> <br>
-
- <div class="container-fluid  well   ">
       <div class="form-group">
-        <p><b>Accesos :</b></p>
+
+        <label for="PU04IDDISTRITO">Distrito al que pertenece: </label>
+        <select class="form-control selectpicker" name="PU04IDDISTRITO" id="PU04IDDISTRITO">
+          <option value="Seleccione">Seleccione...</option>
+          <option value="Nicoya">Nicoya</option>
+          <option value="Masión">Masión</option>
+          <option value="San Antonio">San Antonio</option>
+          <option value="Quebrada Honda">Quebrada Honda</option>
+          <option value="Sámara">Sámara</option>
+          <option value="Nosara">Nosara</option>
+          <option value="Belén">Belén</option>
+        </select>
+
+      </div>
+    </div>
+  </div>
+  <br> <br> <br>
+  <br> <br> <br>
+
+  <div class="container-fluid  well   ">
+    <div class="form-group">
+      <p><b>Accesos :</b></p>
       <?php while ($row = mysqli_fetch_array($result1)):?>
         <label class="checkbox-inline">
           <input type="checkbox" value="<?php echo $row['PU38IDSERVIDUMBRE'];?>" name="pu38servi[]"><?php echo $row['PU38DESCRIPSERVIDUM']; ?>
         </label>
       <?php endwhile; ?>
-      </div>
-      </div><br><br>
+    </div>
+  </div><br><br>
 
 
-       <div class="container-fluid  well   ">
-      <div class="form-group">
-        <p><b>Plan Regulador :</b></p>
+  <div class="container-fluid  well   ">
+    <div class="form-group">
+      <p><b>Plan Regulador :</b></p>
       <?php while ($row = mysqli_fetch_array($result2)):?>
         <label class="checkbox-inline">
           <input type="checkbox" value="<?php echo $row['PU26IDPLAN'];?>" name="pu26planreg[]"><?php echo $row['PU26PLNDESC']; ?>
         </label>
       <?php endwhile; ?>
-      </div>
-      </div><br><br>
+    </div>
+  </div><br><br>
 
-       <div class="container-fluid  well   ">
-      <div class="form-group">
-        <p><b>Nicoya :</b></p>
+  <div class="container-fluid  well   ">
+    <div class="form-group">
+      <p><b>Nicoya :</b></p>
       <?php while ($row = mysqli_fetch_array($result3)):?>
         <label class="checkbox-inline">
           <input type="checkbox" value="<?php echo $row['PU26IDDESCNICOYASAMA'];?>" name="pu26planreg1[]"><?php echo $row['PU26DESCACNICOYASAMA']; ?>
         </label>
       <?php endwhile; ?>
-      </div>
-      </div><br><br>
+    </div>
+  </div><br><br>
 
-      <div class="container-fluid  well   ">
-      <div class="form-group">
-        <p><b>Sámara :</b></p>
+  <div class="container-fluid  well   ">
+    <div class="form-group">
+      <p><b>Sámara :</b></p>
       <?php while ($row = mysqli_fetch_array($result4)):?>
         <label class="checkbox-inline">
           <input type="checkbox" value="<?php echo $row['PU26IDDESCNICOYASAMA'];?>" name="pu26planreg1[]"><?php echo $row['PU26DESCACNICOYASAMA']; ?>
         </label>
       <?php endwhile; ?>
-      </div>
-      </div><br><br>
+    </div>
+  </div><br><br>
 
-      <div class="container-fluid  well   ">
-      <div class="form-group">
-        <p><b>Afectación de Áreas de Protección :</b></p>
+  <div class="container-fluid  well   ">
+    <div class="form-group">
+      <p><b>Afectación de Áreas de Protección :</b></p>
       <?php while ($row = mysqli_fetch_array($result5)):?>
         <label class="checkbox-inline">
           <input type="checkbox" value="<?php echo $row['PU13IDAAP'];?>" name="pu13aap[]"><?php echo $row['PU13DESCAAP']; ?>
         </label>
       <?php endwhile; ?>
-      </div>
-      </div><br><br>
+    </div>
+  </div><br><br>
 
-
-       <tr>
+  <div class="container-fluid  well   ">
+    <tr>
       <td><label class="control-label">Imagen Terreno</label></td>
-        <td><input class="input-group" type="file" name="user_image" accept="image/*" /></td>
+      <td><input class="input-group" type="file" name="user_image" accept="image/*" /></td>
     </tr>
-
-<br><br>
-      <button type="submit" name="btnsave" class="btn btn-success">Guardar Trámite</button>
-      <a id="regresar" class="btn btn-danger" role="button" href="?c=class04ingresotramite&m=index">Regresar</a>
-     </form>
+  </div>
+  <br><br>
+  <button type="submit" name="btnsave" class="btn btn-success">Guardar Trámite</button>
+  <a id="regresar" class="btn btn-danger" role="button" href="?c=class04ingresotramite&m=index">Regresar</a>
+</form>
 
 <br><br><br>
-
-
-
-
-
-
-</div>
-</div>
