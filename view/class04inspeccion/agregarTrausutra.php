0419b990904d7879a72892307bdd4e7bfc0c5267
Mauricio Chévez Gutiérrez <30674987+MauricioChevezG@users.noreply.github.com>
2019-01-06 23:10:05 -0600

    cambios

--- view/class04inspeccion/agregarTrausutra.php
+++ view/class04inspeccion/agregarTrausutra.php
@@ -16,17 +16,15 @@
       <h3>Datos del Inspector:</h3>
 
 
-        <div class="form-group row">
-
-            <label for="PU01CEDUSU">Cédula del Inspector:</label>
-            <input type="text" class="form-control" name="PU01CEDUSU" id="PU01CEDUSU">
-
-        </div>
+      <div class="form-group row">
+        <label for="PU01CEDUSU">Cédula del Inspector:</label>
+        <input type="number" class="form-control" name="PU01CEDUSU" id="PU01CEDUSU" placeholder="Cédula del Inspector" maxlength="11" required>
+      </div>
 
       <button type="submit" class="btn btn-success">Guardar Registro Oficina</button>
       <a id="regresar" class="btn btn-danger" role="button" href="?c=classprincipal&m=index">Regresar</a>
-      </div>
-    </form>
+    </div>
+  </form>
 
 </div>
 <br>
